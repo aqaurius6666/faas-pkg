f902f70e217b7159e2222c8459f5f0242e15638c
Vu Nguyen <aqaurius6666@gmail.com>
2022-09-04 17:00:49 +0700

    init

--- entity/user.go
+++ entity/user.go
@@ -4,4 +4,6 @@ type User struct {
 	ID        int64  `json:"id"`
 	FirstName string `json:"first_name"`
 	LastName  string `json:"last_name"`
+	Username  string `json:"username"`
+	Password  string `json:"password"`
 }
